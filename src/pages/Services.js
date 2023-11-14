1d1b1db55bc9591291f6a7ddec783480bdbb1e83
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-11-14 17:55:14 +0000

    Explore Access without Limits section

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 function Services() {
   return (
     <>
-    <section className="main-section bg-wynkPurple-100">
+    <section className="main-section text-white" style={{ backgroundImage: "url(/images/lady-in-car-hero-image.png)" }}>
       <Header/>
       <section className="scheme-hero-section section-inner-padding flex">
         <div className="w-1/2">
@@ -16,8 +16,17 @@ function Services() {
         </div>
       </section>
     </section>
-    <section className="main-section section-inner-padding bg-wynkOffWhite flex flex-col gap-8 items-start">
+    <section className="main-section section-inner-padding bg-wynkOffWhite">
       <img alt="valuable-services-component" src="/images/valuable-services-component.png"/>
+    </section>
+    <section className="main-section section-inner-padding bg-wynkPurple-100 flex flex-col">
+    <div className="hero-text px-60 flex flex-col gap-4">
+      <h2 className="text-center text-4xl font-bold leading-none">Explore Access without Limits</h2>
+      <p className="text-lg text-center">WynkStars including Consumers, Captains, Merchants, and Businesses can explore boundless access to a variety of services on the Wynk Super App.</p>
+    </div>
+    <div className="flex">
+      
+    </div>
     </section>
     <DownloadWynk/>
     </>
