b6070a6cd31630fe6bdd9d1f96085c9ca9193ebf
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-11-17 09:47:16 +0000

    fix: fonts loading

--- src/components/DownloadWynk.js
+++ src/components/DownloadWynk.js
@@ -7,10 +7,10 @@ function DownloadWynk() {
           <p className=" text-sm sm:text-xl mb-5 w-full pr-8">To download the Wynk super app, use the camera on your phone to scan the QR code. Accessible on iOS and Android platforms.</p>
           <p className="mb-6">Download now at</p>
           <div className="call-to-actions flex gap-4">
-            <a href="https://apps.apple.com/us/app/wynk-super-app/id1668382143">
+            <a href="https://apps.apple.com/us/app/wynk-super-app/id1668382143" target="_blank" rel="noreferrer">
             <img src="/images/AppStore.png" alt="apple app store button"/>
             </a>
-            <a href="https://play.google.com/store/apps/details?id=ng.wynk.wynksupappsupapp&pli=1">
+            <a href="https://play.google.com/store/apps/details?id=ng.wynk.wynksupappsupapp&pli=1" target="_blank" rel="noreferrer">
             <img className="hover:cursor-pointer" src="/images/GooglePlay.png" alt="google play store button"/>
             </a>
           </div>
