cef325cae7af86df26868bd4f23a20e568e84302
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-11-21 19:02:17 +0000

    add: FAQs page

---
+++ src/components/FAQsCards.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+
+export default function FAQsComponent () {
+  const faqs = [
+    {
+      question: "How can i download the app?",
+      answer:
+        "You can get the app by visiting the Google Play Store or iOS store and downloading it using your mobile device. Kindly reach out to us if you’re experiencing any issues.",
+    },
+    {
+      question: "How can i become a captain?",
+      answer:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    },
+    {
+      question: "I was debited for a failed transaction",
+      answer:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+      question: "Are the options for merchants and vendors",
+      answer:
+        "There are many variations of passages of Lorem Ipsum available.",
+    },
+    {
+      question: "I want to become a Wynk Partner",
+      answer:
+        "The standard Lorem Ipsum passage, used since the 1500s, is reproduced for those interested.",
+    },
+    {
+      question: "I have issues trying to login to my app",
+      answer:
+        "The standard Lorem Ipsum passage, used since the 1500s, is reproduced for those interested.",
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const faqsArray = faqs.map((faq, index) => (
+        <div
+          key={index}
+          className="w-full bg-white rounded-lg shadow-md mb-4 p-4"
+        >
+          <div
+            className="flex items-center justify-between cursor-pointer"
+            onClick={() => handleToggle(index)}
+          >
+            <p className="text-[16px] font-semibold">{faq.question}</p>
+            <span className="text-2xl rounded-full h-10 w-10 flex items-center justify-center shadow-lg">
+              {activeIndex === index ? "-" : "+"}
+            </span>
+          </div>
+          {activeIndex === index && (
+            <p className="mt-4 text-gray-600">{faq.answer}</p>
+          )}
+        </div>
+      ));
+
+  return (
+    <div className="w-1/2 mx-auto mt-[-150px] bg-white p-4 rounded-2xl">
+      {faqsArray}
+    </div>
+  );
+};
--- src/pages/FAQs.js
+++ src/pages/FAQs.js
@@ -1,7 +1,30 @@
+import { ContactCard } from "../components/ContactCard";
+import DownloadWynk from "../components/DownloadWynk";
+import FAQsComponent from "../components/FAQsCards";
+import Footer from "../components/Footer";
+import Header1 from "../components/Header1";
+
 export function FAQs () {
   return (
     <>
-    
+      <section className="main-section bg-wynkPurple-100">
+        <Header1/>
+        <section className="section-inner-padding flex flex-col gap-8 items-center justify-center bg-wynkPurple-100">
+            <h2 className="text-[52px] font-bold"><span className="text-wynkPurple-200">Frequently</span> Asked Questions</h2>
+            <p className="w-1/2 text-[18px] text-center">Here are some answers to the frequently asked questions. If you can’t find an answer, contact us.</p>
+            <div className="bg-white w-1/2 p-4 rounded-2xl">
+              <p className="text-[18px] font-bold">Watch a video explainer from Wynk</p>
+            </div>
+        </section>
+      </section>
+      <section className="main-section flex flex-col items-center bg-white">
+        <FAQsComponent/>
+        <ContactCard/>
+      </section>
+      <section className="main-section section-inner-padding bg-white">
+      <DownloadWynk/>
+    </section>
+    <Footer/>
     </>
   );
 }
