f0849aea0a85d15dc406b20fd7024553fd1ac75f
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-12-06 11:05:30 +0000

    update: responsiveness 90% complete

--- src/components/CaptainDashboardModal.js
+++ src/components/CaptainDashboardModal.js
@@ -36,9 +36,9 @@ export default function CaptainDashboardModal() {
   const totalCaptains = 5000;
   const shareOwnershipCaptains = 500;
   return (
-    <div className="p-3 fixed w-screen h-screem bg-[rgba(52,64,84,0.6)] backdrop-blur-[4px] top-0 left-0  z-10 grid place-items-center">
+    <div className="p-3 fixed inset-0 overflow-y-auto w-screen h-screem bg-[rgba(52,64,84,0.6)] backdrop-blur-[4px] top-0 left-0  z-10 grid place-items-center">
       <main className="bg-white w-[100%] md:w-[75%] rounded-[20px]" >
-      <section className="captain-dashboard-main-section flex justify-between">
+      <section className="captain-dashboard-main-section flex flex-col md:flex-row justify-between">
         <div className="flex items-end gap-3">
           <div className="bg-wynkPurple-200 rounded-full w-10 h-10 p-3 flex justify-center items-center text-white">JD</div>
           <div>
@@ -58,7 +58,7 @@ export default function CaptainDashboardModal() {
         </div>
       </section>
       <div className="captain-dashboard-main-section"><hr style={{borderTop: '1px solid #E9E9E9'}}></hr></div>
-      <section className="captain-dashboard-main-section captain-dashboard-metrics flex justify-between">
+      <section className="captain-dashboard-main-section captain-dashboard-metrics flex flex-col gap-4 md:flex-row md:justify-between">
         <div className="captain-dashboard-metrics-card">
           <div>
             <img src="/images/average-rating-icon.png" alt="service icon"/>
@@ -133,8 +133,8 @@ export default function CaptainDashboardModal() {
           </div>
         </div>
       </section>
-      <section className="captain-dashboard-main-section flex justify-between">
-        <section className="flex flex-col gap-4 sm:w-2/5">
+      <section className="captain-dashboard-main-section flex flex-col md:flex-row justify-between">
+        <section className="flex flex-col gap-4 sm:w-2/5 mb-4">
           <p className="font-medium">Captain Details</p>
           <div className="captains-details flex flex-col gap-2">
             <div className="flex justify-between">
